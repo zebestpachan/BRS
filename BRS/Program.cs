45a28700ee5d9aa8965b92d54923a27d7219468d
andr- <andr-@DESKTOP-KT93748>
2022-12-07 15:17:46 +0400

    Добавьте файлы проекта.

---
+++ BRS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Programm
+{
+    public void Main()
+    {
+        //тело
+    }
+    public void Yaroslav()
+    {
+        Console.WriteLine("Yaroslav");
+    }
+    public void Danil()
+    {
+        Console.WriteLine("Danil");
+    }
+}
